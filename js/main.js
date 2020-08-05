41846ba8e698af45b1b91f4d6af474a9fcfdd9b0
rickymwethera <rickymwethera@gmail.com>
2020-08-05 09:34:54 +0300

    added verification function

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function shareIssue (){
+    var name = document.getElementById("names").value;
+    var lastOne = document.getElementById("lastName").value;
+    var carModel = document.getElementById("carselect").value;
+    var issues = document.getElementById("issue").value;
+    var resolve = document.getElementById("fix").value;
+
+    if(name=="") {
+        alert("")
+    };
+        
+};
