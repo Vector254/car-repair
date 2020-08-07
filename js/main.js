f6cf8bb7aaf43bbd3121556c4394167583871135
kabu1 <kkabubenson@gmail.com>
2020-08-07 03:25:02 -0400

    fix bugs

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@ var stateObject = {
    "Mombasa": ["Likoni", "Jomvu","Kisauni"],
    "Kisumu": ["Kisumu east", "Kisumu west","Kisumu central"],
    }
+
+
+
+/*styling the nav bar*/
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+   document.getElementById("mySidebar").style.width = "250px";
+   document.getElementById("main").style.marginLeft = "250px";
+ }
+ 
+ /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+ function closeNav() {
+   document.getElementById("mySidebar").style.width = "0";
+   document.getElementById("main").style.marginLeft = "0";
+ 
  
    }
    window.onload = function () {
